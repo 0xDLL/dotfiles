02a2c17b82c33360322823a8010db0d56627c268
0xDLL <163463472+0xDLL@users.noreply.github.com>
2024-03-15 21:31:42 +0100

    Update rc.lua

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -1,4 +1,6 @@
--- sudo apt install -y picom dmenu flameshot xed git polybar fonts-font-awesome terminator fonts-opensymbol fonts-powerline
+-- sudo apt install -y picom dmenu flameshot xed git polybar fonts-font-awesome terminator fonts-opensymbol fonts-powerline p7zip-full
+-- NERD FONT
+-- mkdir -p ~/.local/share/fonts; cd /dev/shm; wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/SpaceMono.zip; 7z x SpaceMono.zip; mv *.ttf ~/.local/share/fonts; fc-cache -f -v
 
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
@@ -8,7 +10,7 @@ pcall(require, "luarocks.loader")
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
--- Widget and layout library
+-- Widget and layout library 
 local wibox = require("wibox")
 -- Theme handling library
 local beautiful = require("beautiful")
